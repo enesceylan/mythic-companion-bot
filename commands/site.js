7ff1fb0fdd53a4b940615c1dab0abfa96fd6252f
Enes <aenesceylan98@gmail.com>
2022-01-14 13:10:25 +0300

    improved embed catches, converted site command to embed

--- commands/site.js
+++ commands/site.js
@@ -1,5 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports.site = (message) => {
-    message.reply({
-        content: 'https://enesceylan.github.io/mythic-companion/'
+  const homepage = new MessageEmbed()
+    .setColor('#0099ff')
+    .setAuthor({ name: message.author.tag, iconURL: message.author.avatarURL() })
+    .setDescription('Click **[here](https://enesceylan.github.io/mythic-companion/#/)** for Mythic Companion homepage.')
+    .setFooter({
+      text: 'by Mythic Companion',
+      iconURL: 'https://media.discordapp.net/attachments/923331169263702107/930238589256744990/logo192.png',
+    });
+
+  message
+    .reply({
+      embeds: [homepage],
     })
-}
+    .then((msg) => {
+      setTimeout(() => {
+        msg.delete();
+      }, 5000);
+    })
+    .catch(console.error);
+
+  setTimeout(() => {
+    message.delete();
+  }, 5000);
+};
--- functions/embedLink.js
+++ functions/embedLink.js
@@ -1,23 +1,79 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.embedLink = (message) => {
-        //url filter pattern
-    const pattern = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/gm;
-    const link =  message.content.match(pattern);
+  let textArr = message.content.split(' ');
 
-    //split the message to an array, find the index of url, replace url with [link here] and put it back together
-    let textArr = message.content.split(' ');
-    const linkIndex = textArr.findIndex(word => word.match(pattern));
-    let hyperlink = '[[Companion](' + link + ')]';
-    textArr.splice(linkIndex, 1, hyperlink);
-    const text = textArr.join(' ');
+  const heroes = [
+    'Lucifer',
+    'Iset',
+    'Typhon',
+    'Lilith',
+    'Nuwa',
+    'Zeus',
+    'Athena',
+    'Susanoo',
+    'Artemis',
+    'Dionysus',
+    'Thor',
+    'Persephone',
+    'Izanami',
+    'Hades',
+    'Medusa',
+    'Anubis',
+    'Hela',
+    'Hercules',
+    'Lu-Bu',
+    'Ganjiang-&-Moye',
+    'Joan-of-Arc',
+    'Cleopatra',
+    'Cassandra',
+    'Archimedes',
+    'Nagakanya',
+    'Tamamo-no-Mae',
+    'Idun',
+    'Gaia',
+    'Oberon',
+    'Flora',
+  ];
 
-    const websiteEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor({ name: message.author.tag, iconURL: message.author.avatarURL()})
-        .setDescription('**' + text + '**')
-        .setFooter({text: "by Mythic Companion", iconURL: "https://media.discordapp.net/attachments/923331169263702107/930238589256744990/logo192.png"});
+  textArr.forEach((word, index) => {
+    if (word.includes('https://enesceylan.github.io/mythic-companion/#/hero-list')) {
+      const heroLinkArr = word.split('/');
+      if (heroes.includes(heroLinkArr[heroLinkArr.length - 1])) {
+        textArr[index] = `[${heroLinkArr[heroLinkArr.length - 1].split('-').join(' ')}](${word})`;
+      } else {
+        textArr[index] = '[BROKEN LINK]';
+      }
+    } else if (word.includes('https://enesceylan.github.io/mythic-companion/#/team-builder')) {
+      textArr[index] = '[Mythic Companion team builder](' + word + ')';
+    } else if (word.includes('https://enesceylan.github.io/mythic-companion/#/cube')) {
+      textArr[index] = '[Mythic Companion cube of truth](' + word + ')';
+    } else if (word.includes('https://enesceylan.github.io/mythic-companion/#/tierlist')) {
+      textArr[index] = '[Mythic Companion tierlist](' + word + ')';
+    } else if (
+      word === 'https://enesceylan.github.io/mythic-companion' ||
+      word === 'https://enesceylan.github.io/mythic-companion/' ||
+      word === 'https://enesceylan.github.io/mythic-companion//' ||
+      word === 'https://enesceylan.github.io/mythic-companion/#' ||
+      word === 'https://enesceylan.github.io/mythic-companion/#/'
+    ) {
+      textArr[index] = '[Mythic Companion homepage](' + word + ')';
+    } else {
+      textArr[index] = '[BROKEN LINK]';
+    }
+  });
 
-    message.channel.send({ embeds: [websiteEmbed] });
-    message.delete({ timeout: 500});
-}
+  const text = textArr.join(' ');
+
+  const websiteEmbed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setAuthor({ name: message.author.tag, iconURL: message.author.avatarURL() })
+    .setDescription('**' + text + '**')
+    .setFooter({
+      text: 'by Mythic Companion',
+      iconURL: 'https://media.discordapp.net/attachments/923331169263702107/930238589256744990/logo192.png',
+    });
+
+  message.channel.send({ embeds: [websiteEmbed] });
+  message.delete({ timeout: 500 });
+};
